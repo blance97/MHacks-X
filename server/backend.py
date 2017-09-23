876086425a0b3a1586540851b839cd35b1c9e5a1
Ben Brubaker <heavensords1@gmail.com>
2017-09-23 15:23:02 -0400

    working

--- server/backend.py
+++ server/backend.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import JWTManager, jwt_required, create_access_token, ge
 from flask_cors import *
 import googlemaps
 import random
+import json
 from datetime import datetime
 
 gmaps = googlemaps.Client(key='AIzaSyD2A-7qrmxUA4MpTUGojfIhpl2LQF-RF9w')
@@ -20,31 +21,31 @@ def main():
     return 'Hello, World!'
 
 @cross_origin()
-@app.route('/recm', methods=['GET'])
+@app.route('/recm', methods=['POST'])
 def recm():
-    types = request.args.get('types')
-    loc = request.args.get('location')
+    data = json.loads(request.data)
+    types = data['types']
+    loc = data['location']
+    location = (loc['lat'],loc['long'])
     r = random.uniform(0,1)
     places = None
-    if r < types[0].weight:
-        places = gmaps.places_nearby(types[0].type, loc, 24140, type='restaurant')['results']
-    elif r < types[1].weight:
-        places = gmaps.places_nearby(types[1].type, loc, 24140, type='restaurant')['results']
-    elif r < types[2].weight:
-        places = gmaps.places_nearby(types[2].type, loc, 24140, type='restaurant')['results']
+    if r < types[3]['weight']:
+        places = gmaps.places_nearby(keyword=types[3]['type'], location=location, radius=24140, type='restaurant')['results']
+    elif r < types[2]['weight'] + types[3]['weight']:
+        places = gmaps.places_nearby(keyword=types[2]['type'], location=location, radius=24140, type='restaurant')['results']
+    elif r < types[1]['weight'] + types[2]['weight'] + types[3]['weight']:
+        places = gmaps.places_nearby(keyword=types[1]['type'], location=location, radius=24140, type='restaurant')['results']
     else:
-        places = gmaps.places_nearby(types[3].type, loc, 24140, type='restaurant')['results']
+        places = gmaps.places_nearby(keyword=types[0]['type'], location=location, radius=24140, type='restaurant')['results']
 
-
-    s = ""
-    for ob in places:
-        s += ob['name'].replace(u"\u2018", "'").replace(u"\u2019", "'") + ", "
-    return s
+    places.sort(key=sortStuff, reverse=True)
+    placeInfo = gmaps.place(places[0]['place_id'])
+    return jsonify({"place":places[0], "placeInfo":placeInfo})
 
 @cross_origin()
 @app.route('/testbitch', methods=['GET'])
 def testbitch():
-    places = gmaps.places_nearby(keyword="pizza", location=(42.2800266,-83.7471391), radius=24140, type='restaurant')['results']
+    # places = gmaps.places_nearby(keyword="pizza", location=(42.2800266,-83.7471391), radius=24140, type='restaurant')['results']
     # places.sort(key=sortStuff, reverse=True)
     return str(places)
     return str(gmaps.place(places[0]['place_id']))
