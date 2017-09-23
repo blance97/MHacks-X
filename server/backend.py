a0f84fe80443110803d3037219515c4b6a20d61d
Unknown <suvarna.adit@gmail.com>
2017-09-23 15:05:05 -0400

    committed 5678

--- server/backend.py
+++ server/backend.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_cors import *
+from flask_pymongo import PyMongo
 import googlemaps
 import random
+import json
 from datetime import datetime
+from bson import json_util
+from bson import BSON
 
 gmaps = googlemaps.Client(key='AIzaSyD2A-7qrmxUA4MpTUGojfIhpl2LQF-RF9w')
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'food'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/food'
+
 app.debug = True
 app.secret_key = 'secretBATMAN123'
 jwt = JWTManager(app)
+
+mongo = PyMongo(app)
 
 CORS(app)
 
@@ -34,8 +43,6 @@ def recm():
         places = gmaps.places_nearby(types[2].type, loc, 24140, type='restaurant')['results']
     else:
         places = gmaps.places_nearby(types[3].type, loc, 24140, type='restaurant')['results']
-
-
     s = ""
     for ob in places:
         s += ob['name'].replace(u"\u2018", "'").replace(u"\u2019", "'") + ", "
@@ -53,6 +60,26 @@ def testbitch():
         s += ob['name'].replace(u"\u2018", "'").replace(u"\u2019", "'") + ", "
     return s
     return
+
+@app.route('/prefs', methods=['POST'])
+def prefs():
+    pref = mongo.db.pref
+    name = request.json['name']
+    prefs = request.json['prefs']
+    rprefs = request.json['rprefs']
+    grating = request.json['grating']
+    rating = request.json['rating']
+    plevel = request.json['plevel']
+    pref_id = pref.insert({'name': name, 'prefs': prefs, 'rprefs': rprefs, 'grating': grating, 'plevel': plevel, 'rating': rating})
+    new_pref = pref.find_one({'_id': pref_id})
+    output = {'name' : new_pref['name'], 'prefs': new_pref['prefs'], 'rprefs': new_pref['rprefs'], 'grating': new_pref['grating'], 'plevel': new_pref['plevel'], 'rating': new_pref['rating']}
+    return jsonify({'result': output})
+
+@app.route('/train', methods=['GET'])
+def train():
+    pref = mongo.db.pref
+    
+    return jsonify(json.loads(json_util.dumps(pref.find({}))))
 
 def sortStuff(json):
     try:
