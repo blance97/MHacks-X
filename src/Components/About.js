44058c71b7716bc3ebf65a3fbaf7d2434ca493c9
Lance Dinh <lancedinh7@gmail.com>
2017-09-24 03:03:50 -0400

    michael edit aboutm e

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,21 +10,25 @@ export default class About extends Component {
                 <Container text style={{ color: "white" }}>
                     <Header as='h1' style={{ color: "white" }}>What is Food Poll?</Header>
                     <ul>
-                        <p>This is a solution for people in groups that need to decide to get food based off multiple people's opinion. It gathers everyones opinion so that the poll creator can determine what type of food to get.</p>
-                        <p>Firebase is used as the "back-end", so results come in live. Geolocation is used to find suggested places near you (Using Google places API). </p>
-                        <p>There is also mobile apps for this application for both IOS and Android. It is developed, but I need to do some refining and see if its worth releasing.</p>
+                        <p>This is a solution for people in groups that need to decide to get food based off multiple people's opinion. Everytime the button "Give me a recommendation!" is clicked, The app generates a new suggested place. It gathers everyones opinion and generates a suggested place based off their preferences.</p>
+                        <p>Tensorflow is used to learn to create better suggestions based off the rating feedback given. Firebase is utilized for the live data and its NoSQL database. Geolocation is used to find the base suggested places near you (Using Google places API). </p>
+                        <p>For the Backend, we use python flask and MongoDB.</p>
                     </ul>
                     <div>
-                        <Button as='a' href="https://github.com/blance97/foodpoll" inverted color='black' content='View Code' icon='github' labelPosition='left' />
+                        <Button as='a' href="https://github.com/blance97/MHacks-X" inverted color='black' content='View Frontend Code' icon='github' labelPosition='left' />
+                        <Button as='a' href="https://github.com/brubakbd/MHacks-X" inverted color='black' content='View Backend Code' icon='github' labelPosition='left' />
                     </div>
                     <h2 style={{ textAlign: "left" }}>Flow: </h2>
                     <ul style={{ textAlign: "left" }}>
-                        <li>Create Poll(Survey)</li>
-                        <li>Copy Link</li>
-                        <li>Send link to friends/co-workers</li>
-                        <li>Vote on food and select preference</li>
-                        <li>Get results and view suggested places to get food</li>
+                        <li>Create Preference page</li>
+                        <li>Share Preference page with friends/co-workers</li>
+                        <li>Select preferences</li>
+                        <li>Get results and view the suggested place.</li>
                     </ul>
+                    <div>
+                        <b>Created By:</b>
+                        <b style={{fontFamily:"italic"}}> Lance Dinh,  Michael Crowell, Ben Brubaker, Adit Suvarna </b>
+                    </div>
                 </Container>
             </Segment>
         )
